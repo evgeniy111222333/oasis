477073f98cea8382bdc3d1efe634267af2dc458b
evgeniy111222333 <itsdelka001@ukr.net>
2026-06-26 22:28:51 +0300

    Fix script RAM variable expansion

--- run.ps1
+++ run.ps1
@@ -5,7 +5,7 @@ $RAM = "4G"
 
 Write-Host "Starting Minecraft Server with $RAM RAM Allocation..." -ForegroundColor Cyan
 
-java -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1ReservePercent=15 -XX:G1HeapRegionSize=32m -XX:G1MixedGCCountTarget=8 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxGCPauseMillis=200 -jar server.jar nogui
+java "-Xms$RAM" "-Xmx$RAM" -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1ReservePercent=15 -XX:G1HeapRegionSize=32m -XX:G1MixedGCCountTarget=8 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxGCPauseMillis=200 -jar server.jar nogui
 
 Write-Host "Server stopped." -ForegroundColor Yellow
 Pause
